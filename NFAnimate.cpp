9b354b72f74660df7e1d0bd4666d1128bea30326
arroyoruizj <arroyoruizj@carleton.edu>
2025-04-09 09:37:19 -0500

    Starting code for neighbor finder lab

--- NFAnimate.cpp
+++ NFAnimate.cpp
@@ -1,14 +1,44 @@
 #include "emp/web/Animate.hpp"
 #include "emp/web/web.hpp"
+#include "emp/math/math.hpp"
 
 emp::web::Document doc{"target"};
 
 class NFAnimator : public emp::web::Animate {
+
+    emp::web::Canvas canvas{100, 100, "canvas"};
+    
+    public:
+
+        NFAnimator() {
+            doc << canvas;
+            
+            for(int x = 0; x < 10; x++) {
+
+                for(int y = 0; y < 10; y++) {
+
+                    canvas.Rect(x * 10, y * 10, 10, 10, "white", "black");
+
+                }
+            }
+
+        }
+
+        void FindNeighbors(int x, int y) {
+            // This function will find the neighbors of a cell
+            // and update the canvas accordingly
+            canvas.Rect(x * 10, y * 10, 10, 10, "black", "black");
+        }
+
+        
 
 };
 
 NFAnimator animator;
 
 int main() {
+
+    animator.FindNeighbors(5, 5);
     animator.Step();
+    
 }
